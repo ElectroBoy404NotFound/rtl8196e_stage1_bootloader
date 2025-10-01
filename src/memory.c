08ac4ac79e68b198295ad3a4f1667201a507ec18
Nikunj Doke <mwnjele@gmail.com>
2025-10-01 15:55:50 +0000

    latest code

--- src/memory.c
+++ src/memory.c
@@ -1,11 +1,11 @@
-// #include "stdint.h"
-// #include "stddef.h"
+#include "stdint.h"
+#include "stddef.h"
 
-// void *memcpy(void *dest, const void *src, size_t n) {
-//     char *d = (char *)dest;
-//     const char *s = (const char *)src;
-//     for (size_t i = 0; i < n; i++) {
-//         d[i] = s[i];
-//     }
-//     return dest;
-// }
+void *memcpy(void *dest, const void *src, size_t n) {
+    char *d = (char *)dest;
+    const char *s = (const char *)src;
+    for (size_t i = 0; i < n; i++) {
+        d[i] = s[i];
+    }
+    return dest;
+}
